3790e10f560cc13a3c5aa104e883786319324eee
kouseina <daffa@diyo.app>
2024-12-08 12:44:30 +0700

    feat(project): rename

--- obj/Debug/net8.0-windows/BimbelUn.AssemblyInfo.vb
+++ obj/Debug/net8.0-windows/BimbelUn.AssemblyInfo.vb
@@ -13,12 +13,12 @@ Option Explicit On
 
 Imports System
 Imports System.Reflection
-<Assembly: System.Reflection.AssemblyCompanyAttribute("EvaluasiMahasiswa"),  _
+<Assembly: System.Reflection.AssemblyCompanyAttribute("BimbelUn"),  _
  Assembly: System.Reflection.AssemblyConfigurationAttribute("Debug"),  _
  Assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0"),  _
  Assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d5cf4cc54bee175ddf610d62c5e41a44315826b3"),  _
- Assembly: System.Reflection.AssemblyProductAttribute("EvaluasiMahasiswa"),  _
- Assembly: System.Reflection.AssemblyTitleAttribute("EvaluasiMahasiswa"),  _
+ Assembly: System.Reflection.AssemblyProductAttribute("BimbelUn"),  _
+ Assembly: System.Reflection.AssemblyTitleAttribute("BimbelUn"),  _
  Assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0"),  _
  Assembly: System.Runtime.Versioning.TargetPlatformAttribute("Windows7.0"),  _
  Assembly: System.Runtime.Versioning.SupportedOSPlatformAttribute("Windows7.0")> 
